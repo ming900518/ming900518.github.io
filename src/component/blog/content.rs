6559f3f8bcf8b64ed4e6cf7616696a10af298a5c
Ming Chang <mail@mingchang.tw>
2023-01-30 10:30:03 +0800

    yew: fix css for article.

--- src/component/blog/content.rs
+++ src/component/blog/content.rs
@@ -89,7 +89,7 @@ pub fn content(props: &Props) -> HtmlResult {
 
     return Ok(html! {
         <>
-        <main id="main" class=" bg-image" style="background-image: url(/assets/img/bg.webp);">
+        <main id="main" class="bg-image" style="background-image: url(/assets/img/bg.webp);">
         <div class="intro intro-single route">
             <div class="overlay-mf"></div>
             <div class="intro-content display-table">
@@ -113,8 +113,10 @@ pub fn content(props: &Props) -> HtmlResult {
                         <div id="contact" class="box-shadow-full">
                             <div class="row">
                                 <div class="col-sm-12">
-                                    <div class="title-box" id="articleContent">
+                                    <div class="article-content">
+                                    <div class="title-box" id="article-content">
                                         {Html::from_html_unchecked(blog_article_content.content.clone().into())}
+                                    </div>
                                     </div>
                                 </div>
                             </div>
